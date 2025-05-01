e8b9f8fa0db31b17c5fcd4fcfd4c167a7f7092d4
LGH <leeganghee4954@naver.com>
2025-05-01 22:13:46 +0900

    feat: 결과 페이지 레이아웃 중간저장(버튼 hover active 효과 추가 필요)

--- app/(provider)/(root)/play/page.tsx
+++ app/(provider)/(root)/play/page.tsx
@@ -2,28 +2,9 @@ import React from "react";
 
 function PlayPage() {
   return (
-    <header className="bg-white w-full h-screen fixed">
-      <div className="bg-yellow-300 flex items-center gap-x-50">
-        <div className="pl-20 py-5 flex items-center gap-x-5">
-          <div>
-            <img
-              src="/images/Mango_icon.png"
-              alt="Mango_icon"
-              className="w-20"
-            />
-          </div>
-          <div>
-            <h1 className="text-gray-800 font-bold text-3xl">MANGO</h1>
-          </div>
-        </div>
-        <div className="bg-white border-2 border-neutral-500 flex items-center gap-x-120 border-b border-t border-b-neutral-500 rounded-sm">
-          <p className="text-neutral-400 py-3 pl-3 pr-3">
-            월드컵 제목 또는 인물 이름으로 검색하세요.
-          </p>
-          <div className="bg-neutral-600 p-3  ">검색</div>
-        </div>
-      </div>
-    </header>
+    <div>
+      <h1>Play</h1>
+    </div>
   );
 }
 
