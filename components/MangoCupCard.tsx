8f7aefe07d79503f165ff84896549e283eb6aed4
LGH <leeganghee4954@naver.com>
2025-05-04 23:58:44 +0900

    feat: 수파베이스 데이터 연결됨

--- components/MangoCupCard.tsx
+++ components/MangoCupCard.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { SlOptionsVertical } from "react-icons/sl";
 
-function MangoCupCard() {
+interface MangoCupCardProps {
+  title: string;
+  likeCount: number;
+  createdAt?: string;
+}
+
+function MangoCupCard({ title, likeCount }: MangoCupCardProps) {
   return (
     <div className="w-[430px] bg-white rounded-3xl p-3 shadow-md">
       <div className="w-full aspect-video bg-gray-500 rounded-2xl"></div>
       <div className="mx-3 mt-3 flex items-center justify-between">
         <div>
-          <p className="text-2xl">우리집에 고양이 보고갈래?</p>
-          <p className="mt-1 text-xs">좋아요 5.8k 6일전</p>
+          <p className="text-2xl">{title}</p>
+          <p className="mt-1 text-xs">{likeCount} 6일전</p>
         </div>
         <SlOptionsVertical className="text-black w-5 h-5" />
       </div>
