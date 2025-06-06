fa0f24ffa77ee04987954c18b9cb4dd7fb3e3d6c
LGH <leeganghee4954@naver.com>
2025-06-06 18:37:49 +0900

    feat: 헤더 디자인 수정

--- app/(provider)/(root)/layout.tsx
+++ app/(provider)/(root)/layout.tsx
@@ -1,24 +1,28 @@
+import Link from "next/link";
 import React, { PropsWithChildren } from "react";
+import { IoSearch } from "react-icons/io5";
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
       <header className="bg-primary w-full h-[100px] fixed flex items-center justify-center">
-        <img
-          src="/images/mangoCupLogo.png"
-          alt="logo"
-          className="left-40 h-10 absolute"
-        />
-        <input
-          className="w-[800px] h-10 border-1 rounded-l-sm border-secondary text-[#6666666] pl-3 bg-white outline-0"
-          type="text"
-          placeholder="월드컵 제목 또는 인물 이름으로 검색하세요."
-        />
-        <button className="h-10 px-3 bg-secondary rounded-r-sm text-white font-bold">
-          검색
-        </button>
+        <div className="h-10 mr-auto cursor-pointer ml-12 shrink-0">
+          <Link href={"/"}>
+            <img src="/images/mangoCupLogo.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="h-11 flex w-[40%] mr-auto -translate-x-20 shrink-1 rounded-full bg-[#fff] shadow-md focus-within:brightness-95 duration-200">
+          <input
+            className="flex-1 h-full text-[#777] text-lg font-medium pl-[18px] outline-0"
+            type="text"
+            placeholder="월드컵 제목 또는 인물 이름으로 검색하세요."
+          />
+          <button className="shrink-0 h-full pr-4 text-[#aaa] font-bold text-2xl">
+            <IoSearch />
+          </button>
+        </div>
       </header>
-      <main className="pt-[100px]">{children}</main>
+      <main className="pt-[100px] bg-[#f6f6f6]">{children}</main>
     </>
   );
 }
